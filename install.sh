c6463e09dc55cbacc44f4bafe9da42c55cd3945d
dk808 <41762330+dk808@users.noreply.github.com>
2020-03-10 03:25:38 -0700

    remove carriage return

--- install.sh
+++ install.sh
@@ -485,7 +485,7 @@ function status_loop() {
 	sudo chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"
 	NUM='30'
 	MSG1="${CYAN}Refreshes every 30 seconds while syncing to chain. Refresh loop will stop automatically once it's fully synced.${NC}"
-	MSG2="\e[2K\r"
+	MSG2=''
 	spinning_timer
 	if [[ $(wget -nv -qO - https://explorer.zel.cash/api/status?q=getInfo | jq '.info.blocks') == $(${COIN_CLI} getinfo | jq '.blocks') ]]; then
 	    break
