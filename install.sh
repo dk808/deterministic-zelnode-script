c61ea2ed9e28a7f4d3561efe908f938b9c37e79c
dk808 <41762330+dk808@users.noreply.github.com>
2020-04-01 01:41:45 -0700

    fix banner function and remove function not used

--- install.sh
+++ install.sh
@@ -373,19 +373,6 @@ EOF
     sudo chown root:root /etc/logrotate.d/zeldebuglog
 }
 
-function kill_sessions() {
-    echo -e "${YELLOW}If you have made a previous run of the script and have a session running for Zelflux it must be removed before starting a new one."
-    echo -e "${YELLOW}Detecting sessions please remove any that is running Zelflux...${NC}" && sleep 5
-    tmux ls | sed -e 's/://g' | cut -d' ' -f 1 | tee tempfile > /dev/null 2>&1
-    grep -v '^ *#' < tempfile | while IFS= read -r line
-    do
-        if whiptail --yesno "Would you like to kill session ${line}?" 8 43; then
-	    tmux kill-sess -t "$line"
-	fi
-    done
-    rm tempfile
-}
-
 function install_zelflux() {
     echo -e "${YELLOW}Detect OS version to install Mongodb, Nodejs, and updating firewall to install Zelflux...${NC}"
     sudo ufw allow $ZELFRONTPORT/tcp
@@ -575,7 +562,7 @@ function display_banner() {
     echo -e "${PIN} ${YELLOW}To update binaries wait for announcement that update is ready then enter:${NC} ${SEA}./${UPDATE_FILE}${NC}"
     echo
     echo -e "${YELLOW}   PM2 is now managing Zelflux to start up on reboots.${NC}"
-    pm list
+    pm2 list
     echo
     echo -e "${PIN} ${CYAN}To access your frontend to Zelflux enter this in as your url: ${SEA}${WANIP}:${ZELFRONTPORT}${NC}"
     echo -e "${YELLOW}================================================================================================================================${NC}"
